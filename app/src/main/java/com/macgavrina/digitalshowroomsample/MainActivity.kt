948cd58cb53c0477f910310451aa8c7775cfe085
Irina Gavrina <ivgavrina@gmail.com>
2019-07-10 20:11:19 +0300

    Initial commit

---
+++ app/src/main/java/com/macgavrina/digitalshowroomsample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.macgavrina.digitalshowroomsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
